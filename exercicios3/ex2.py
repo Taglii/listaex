7d76ec2dfc1e3c7e47e8350ad3db4c9878f0690d
Matheus Tagliatti <163213259+Taglii@users.noreply.github.com>
2024-09-16 21:39:41 -0300

    Update ex2.py

--- exercicios3/ex2.py
+++ exercicios3/ex2.py
@@ -1,12 +1,12 @@
-maior = 0
-menor = 100000000
-
 for i in range(20):
     n = int(input('Informe um numero:'))
+    if i == 0:
+        menor = n
+        maior = n
+    if n < menor:
+        menor = n
     if n > maior:
         maior = n
-    elif n < menor:
-        menor = n
 
 print('Maior numero:', maior)
 print('Menor numero', menor)
