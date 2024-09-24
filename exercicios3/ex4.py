cac165b641f2cf54ef6b13349a1b53466c47cb11
Matheus Tagliatti <163213259+Taglii@users.noreply.github.com>
2024-09-23 22:07:20 -0300

    ex4 completo

--- exercicios3/ex4.py
+++ exercicios3/ex4.py
@@ -9,27 +9,31 @@
 soma_salario = 0
 habitantes = 0
 filhos = 0
-maior_salario = 0
-mais_1000 = 0
+menos_1000 = 0
 
 salario = float(input('Informe o salario: '))
+maior_salario = salario
 while salario >= 0:
     if salario > 0:
         if salario > maior_salario:
             maior_salario = salario
-        if salario >= 1000:
-            mais_1000 += 1
-        habitantes += 1 
-        soma_salario += salario
+        if salario <= 1000:
+            menos_1000 += 1
+    habitantes += 1 
+    soma_salario += salario
     
     
-    filhos += int(input('Informe o numero de filhos: '))
+    filhos = int(input('Informe o numero de filhos: '))
+    if filhos < 0:
+         filhos = int(input('Informe um numero de filhos possitivo ou zero de filhos: '))
+    soma_filho += filhos 
     salario = float(input('Informe o salario: '))
     
 if habitantes > 0:
     media_salario = soma_salario/habitantes
-    media_filhos = filhos/habitantes
-    por_1000 = (mais_1000/habitantes) * 100
+    
+    media_filhos = soma_filho/habitantes
+    por_1000 = (menos_1000/habitantes) * 100
     
     print('Media salarial: {:.2f}'.format(media_salario))
     print('Media de filhos por habitantes: {}'.format(int(media_filhos)))
